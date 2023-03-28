44d103f594c9c10ca24befdc51d3f15d0b4db234
Julian Calle Perez <juliancp17@gmail.com>
2023-03-28 06:43:44 -0500

    y7huji

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignacionesComponent } from './asignaciones/asignaciones.component';
+import { AulasComponent } from './aulas/aulas.component';
+import { CompetenciasComponent } from './competencias/competencias.component';
+import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MateriasComponent } from './materias/materias.component';
+import { ProfesoresComponent } from './profesores/profesores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:InicioComponent
+  },
+  {
+    path:'profesores',
+    component:ProfesoresComponent
+  },
+  {
+    path:'estudiantes',
+    component:EstudiantesComponent
+  },
+  {
+    path:'materias',
+    component:MateriasComponent
+  },
+  {
+    path:'competencias',
+    component:CompetenciasComponent
+  },
+  {
+    path:'aulas',
+    component:AulasComponent
+  },
+  {
+    path:'asignaciones',
+    component:AsignacionesComponent
+  },
+  {
+    path:'**',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './static/navbar/navbar.component';
+import { FooterComponent } from './static/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
